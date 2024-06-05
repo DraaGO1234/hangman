ee4d8eaa9f0cdb33b106e6f06d2dcd0aabc90c47
Tarcea Nicolae Bogdan <bogdantarcea1@gmail.com>
2024-06-05 11:21:11 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+
+<h1>Hangman!</h1>
+<p>Find the hidden word - Enter a letter</p>
+
+    </div>
+  )
+}
+
+export default Header
